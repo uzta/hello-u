163173a14e841b4fcdcf68be2d0a66ebec68948e
julian@uzta.org <julian@uzta.org>
2024-07-10 11:13:23 -0600

    Added personal site

---
+++ src/app/julian/page.tsx
@@ -0,0 +1,19 @@
+import ContactInfoJulian from "../components/ContactInfoJulian";
+
+export default async function Page() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      {/* Desktop message */}
+      <div className="absolute inset-0 z-10 bg-gradient-to-t from-black to-slate-950 opacity-50 flex items-center justify-center hidden md:flex">
+        <p className="text-white text-4xl font-bold text-center">
+          Please visit this site on a mobile phone
+        </p>
+      </div>
+      
+      {/* Mobile-specific content */}
+      <div className="md:hidden">
+          <ContactInfoJulian/>
+      </div>
+    </div>
+  );
+}
